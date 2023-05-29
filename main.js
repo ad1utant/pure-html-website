494e46f2d7f9ae989ef328660a2859b5fc6006d5
adiutant <mikolaj@marasek.eu>
2023-05-29 14:45:01 +0200

    'nav'

--- main.js
+++ main.js
@@ -1,11 +1,31 @@
-let i = 0;
-let txt = 'test';
-let typeWirte = false
-function typeWriter() {
-while (x>1){
-    if (i < txt.length) {
-        document.getElementById("type").innerHTML += txt.charAt(i);
-        i++;
-        setTimeout(typeWriter, 50);
+
+function getCurrentSection(scrollPosition){
+    const sections = document.querySelectorAll("section");
+    for (const section of sections) {
+        const sectionTop = section.offsetTop;
+        const sectionHeight = section.offsetHeight;
+        if (scrollPosition >= sectionTop && scrollPosition < sectionTop + sectionHeight) {
+            return section.id;
+        }
     }
-}}
+}
+
+
+window.addEventListener('scroll', () => {
+    const scroll = window.scrollY;
+    console.log(scroll);
+    const currentSection = getCurrentSection(scroll);
+    console.log(currentSection);
+    const navElement = document.getElementById('nav');
+    if (currentSection === 'home') {
+        navElement.style.backgroundColor = '#0099ff';
+    } else if (currentSection === 'projects' || currentSection === 'projects_wave') {
+        navElement.style.backgroundColor = 'white';
+    } else if (currentSection === 'contact' || currentSection === 'contact_wave') {
+        navElement.style.backgroundColor = 'white';
+
+    } else if(currentSection === 'about' || currentSection === 'about_wave'){
+        navElement.style.backgroundColor = '#ff5500';
+    }
+    navElement.style.transition = 'background-color 0.3s ease-in-out';
+});
